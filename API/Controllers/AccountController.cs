5d631e17c515b4fae722c1c0dabb763fc0640cc8
Amjed <amjed1718@gmail.com>
2020-12-18 16:44:50 +0200

    [API]add login method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -8,28 +8,34 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 namespace API.Controllers
 {
-   public class AccountController : BaseApiController
-   {
-      private readonly DataContext _context;
-      public AccountController(DataContext context)
-      {
-         _context = context;
-      }
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
 
-      [HttpPost("register")]
-      public async Task<ActionResult<AppUser>> Register(string username,string password)
-      {
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(DTOs.RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
             using var hmac = new HMACSHA512();
 
-            var user=new AppUser
+            var user = new AppUser
             {
-               UserName=username,
-               PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
-               PasswordSalt=hmac.Key
+                UserName = registerDto.Username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return user;
-      }
-   }
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
 }
